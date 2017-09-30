46fcfc9ae29f10feee770c27145b10932a789f4f
JIeIIIa <stealth@bigmir.net>
2017-10-01 01:26:52 +0300

    Add header on index.html
    +inclusion Bootstrap
    +add /css and /libs folders

--- src/main/java/ua/kiev/prog/photopond/configuration/ApplicationConfig.java
+++ src/main/java/ua/kiev/prog/photopond/configuration/ApplicationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
@@ -38,5 +39,12 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
         interceptor.setParamName("lang");
         registry.addInterceptor(interceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/libs/**").addResourceLocations("/libs/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
     }
 }
