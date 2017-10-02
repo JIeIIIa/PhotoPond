38f9f64c7ec6ccf0d79d9c15913cdd5716fa3fc8
JIeIIIa <stealth@bigmir.net>
2017-10-02 12:27:33 +0300

    Initialize WebSecurity

---
+++ src/main/java/ua/kiev/prog/photopond/configuration/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package ua.kiev.prog.photopond.configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static Logger log = LogManager.getLogger(SecurityConfiguration.class);
+
+
+}
