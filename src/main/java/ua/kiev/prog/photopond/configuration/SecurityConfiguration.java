2b6f08e2946ecef1084e1213e488c27eafe870c2
JIeIIIa <stealth@bigmir.net>
2017-10-02 16:07:20 +0300

    Add SecurityConfiguration for configure SpringSecurity

--- src/main/java/ua/kiev/prog/photopond/configuration/SecurityConfiguration.java
+++ src/main/java/ua/kiev/prog/photopond/configuration/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package ua.kiev.prog.photopond.configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,11 +16,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static Logger log = LogManager.getLogger(SecurityConfiguration.class);
 
     @Autowired
-    @Qualifier("userDetailsServiceImpl")
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    public void registerGlobalAuthentication(AuthenticationManagerBuilder auth, UserDetailsService userDetailsService) throws Exception {
         auth
                 .userDetailsService(userDetailsService);
     }
