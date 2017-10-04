e61d2441259fc6b6ce87c01c85a7f84ec94be90a
JIeIIIa <stealth@bigmir.net>
2017-10-04 18:15:24 +0300

    Add Spring Security configuration

--- src/main/java/ua/kiev/prog/photopond/configuration/SecurityConfiguration.java
+++ src/main/java/ua/kiev/prog/photopond/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,16 +18,46 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void registerGlobalAuthentication(AuthenticationManagerBuilder auth, UserDetailsService userDetailsService) throws Exception {
+        log.debug("Setup UserDetailsService in AuthenticationManagerBuilder");
         auth
                 .userDetailsService(userDetailsService);
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        log.debug("Configure WebSecurity: add ignoring paths");
+        web.ignoring()
+                .antMatchers("/css/**")
+                .antMatchers("/libs/**")
+                .antMatchers("/login");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+        log.debug("Configure HttpSecurity.");
         super.configure(http);
-        http
-                .csrf().disable()
+
+        http.csrf()
+                .disable()
                 .authorizeRequests()
-                .antMatchers("/*").hasAnyRole("USER", "ADMIN");
+                .antMatchers("/**").hasAnyRole("USER", "ADMIN")
+                .and();
+        http.exceptionHandling()
+                .accessDeniedPage("/unauthorized")
+                .and();
+        http.formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error=true")
+                .loginProcessingUrl("/j_spring_security_check")
+                .usernameParameter("j_login")
+                .passwordParameter("j_password")
+                .permitAll()
+                .and();
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
+                .invalidateHttpSession(true);
     }
 }
