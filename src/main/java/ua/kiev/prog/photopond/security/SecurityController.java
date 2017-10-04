830bba2f13dd3da578e6258bacd79dcb0d37b83a
JIeIIIa <stealth@bigmir.net>
2017-10-04 18:32:36 +0300

    Refactoring: Security controller
    Create SecurityController and remove some logic from WelcomeController.

--- src/main/java/ua/kiev/prog/photopond/security/SecurityController.java
+++ src/main/java/ua/kiev/prog/photopond/security/SecurityController.java
@@ -1,18 +1,18 @@
-package ua.kiev.prog.photopond.core;
+package ua.kiev.prog.photopond.security;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.kiev.prog.photopond.core.WelcomeController;
 
 @Controller
-public class WelcomeController {
+public class SecurityController {
     private static Logger log = LogManager.getLogger(WelcomeController.class);
 
-    @RequestMapping(value = {"/", "index"})
-    public String index() {
-        log.debug("Request to \"/\"");
-        return "index";
+    @RequestMapping("/login")
+    public String login() {
+        log.debug("Request to /login");
+        return "login";
     }
-
 }
