50c3b13550beb53bb139865e8459d53876eae286
JIeIIIa <stealth@bigmir.net>
2017-10-05 17:38:12 +0300

    Change MainExceptionController.directoryAccessDenied()
    + add output session attributes (login, url)

--- src/main/java/ua/kiev/prog/photopond/exception/MainExceptionController.java
+++ src/main/java/ua/kiev/prog/photopond/exception/MainExceptionController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
 @ControllerAdvice
 public class MainExceptionController {
     private static Logger log = LogManager.getLogger(MainExceptionController.class);
@@ -15,8 +19,13 @@ public class MainExceptionController {
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ResponseBody
-    public String directoryAccessDenied() {
+    public String directoryAccessDenied(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Map<String, String> attributes = (Map<String, String>) session.getAttribute("attributeMap");
 
-        return "Happy access denied!";
+        String loginUser = attributes.get("userLogin");
+        String url = attributes.get("url");
+        log.warn("directoryAccessDenied ->   url: " + url + "      access denied for userLogin = '" + loginUser + "'");
+        return "Happy access denied!   userLogin = " + loginUser + "   url: " + url;
     }
 }
