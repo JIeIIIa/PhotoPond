6c53ba061ab7eddf21435d8c02de5a729bedcc84
JIeIIIa <stealth@bigmir.net>
2017-10-12 17:12:34 +0300

    Add tests for testing EmbeddedUserInfoRepository

---
+++ src/test/java/ua/kiev/prog/photopond/user/EmbeddedUserInfoRepositoryTest.java
@@ -0,0 +1,73 @@
+package ua.kiev.prog.photopond.user;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class EmbeddedUserInfoRepositoryTest {
+    private static final String USER_LOGIN = "user";
+    private EmbeddedUserInfoRepository embeddedUserInfoRepository;
+    private UserInfo testUser = null;
+
+    @Before
+    public void setUp() throws Exception {
+        testUser = new UserInfo(USER_LOGIN, "user", UserRole.USER);
+        UserInfo first = new UserInfo("someUser", "password", UserRole.USER);
+        UserInfo second = new UserInfo("admin", "admin", UserRole.USER);
+
+        embeddedUserInfoRepository = new EmbeddedUserInfoRepository(Arrays.asList(testUser, first, second));
+    }
+
+    @Test
+    public void checkUserByIDSuccessTest() {
+        assertThat(embeddedUserInfoRepository.existByLogin(USER_LOGIN))
+                .isTrue();
+    }
+
+    @Test
+    public void notExistUserTest() {
+        assertThat(embeddedUserInfoRepository.existByLogin("UnknownUser"))
+                .isFalse();
+    }
+
+    @Test
+    public void findUserSuccessTest() {
+        assertThat(embeddedUserInfoRepository.findByLogin(USER_LOGIN))
+                .isNotNull()
+                .isEqualTo(testUser)
+        ;
+    }
+
+    @Test
+    public void findUserWithNullLoginTest() {
+        assertThat(embeddedUserInfoRepository.findByLogin(null))
+                .isNull();
+    }
+
+    @Test
+    public void findUserFailureTest() {
+        assertThat(embeddedUserInfoRepository.findByLogin("UnknownUser"))
+                .isNull();
+    }
+
+    @Test
+    public void addUserTest() {
+        final String anotherUserLogin = "anotherUser";
+        UserInfo anotherUser = new UserInfo(anotherUserLogin, "pass", UserRole.USER);
+
+        embeddedUserInfoRepository.addUser(anotherUser);
+
+        assertThat(embeddedUserInfoRepository.existByLogin(anotherUserLogin))
+                .isTrue();
+        assertThat(embeddedUserInfoRepository.findByLogin(anotherUserLogin))
+                .isNotNull()
+                .isEqualTo(anotherUser);
+    }
+}
+
